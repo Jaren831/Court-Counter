0c113510ae7fbc76e18bd109288e147fdfce0849
Jaren831 <jaren.lynchmobb@gmail.com>
2016-06-25 09:50:03 -0700

    App good to go. - Fixed functionality allowing number buttons to add to score - Reset button resets score - Removes hardcoded strings. Relevant information now in string file. - Adds scrollable view. Now looks good in landscape.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.courtcounter;
 
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int score_one = 0;
+    int score_two = 0;
+    public void one1(View view) {
+        score_one += 1;
+        displayScore1(score_one);
+    }
+    public void two1(View view) {
+        score_one += 2;
+        displayScore1(score_one);
+    }
+    public void three1(View view) {
+        score_one += 3;
+        displayScore1(score_one);
+    }
+    public void one2(View view) {
+        score_two += 1;
+        displayScore2(score_two);
+    }
+    public void two2(View view) {
+        score_two += 2;
+        displayScore2(score_two);
+    }
+    public void three2(View view) {
+        score_two += 3;
+        displayScore2(score_two);
+    }
+    public void reset(View view) {
+        resetScore(0);
+    }
+    private void resetScore(int number) {
+        score_one = 0;
+        score_two = 0;
+        displayScore1(score_one);
+        displayScore2(score_two);
+    }
+    private void displayScore1(int number) {
+        TextView score1TextView = (TextView) findViewById(R.id.score1);
+        score1TextView.setText(String.valueOf(number));
+    }
+    private void displayScore2(int number) {
+        TextView score2TextView = (TextView) findViewById(R.id.score2);
+        score2TextView.setText(String.valueOf(number));
+    }
+
 }
